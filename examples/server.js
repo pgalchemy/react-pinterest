8ab149f03471087d7ca27efb42844bb6aa1b6046
Joshua Rose <joshuarose@outlook.com>
2018-05-22 16:53:38 -0400

    Made minimum changes to get webpack examples to compile

--- examples/server.js
+++ examples/server.js
@@ -4,9 +4,9 @@ var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+    hot: false,
+    historyApiFallback: true,
+}).listen(3000, 'localhost', function(err, result) {
     if (err) {
         console.log(err);
     }
